4ffc783fa9ac580ed2050da38af5d5cf4c01ab00
joelchongg <joelchongggg@gmail.com>
2025-10-10 11:03:45 +0800

    Add support for sorting command
    Modified the address book to be able to sort contacts
    Contacts can be sorted in ascending or descending order
    based on name or tags
    
    When sorting based on tags, it will sort the tags
    for each contact, and take the first tag to be
    compared.

--- src/main/java/seedu/address/logic/commands/SortCommand.java
+++ src/main/java/seedu/address/logic/commands/SortCommand.java
@@ -2,8 +2,11 @@ package seedu.address.logic.commands;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
+import java.util.Comparator;
+
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
 
 /**
  * Sorts all persons in the address book for the user.
@@ -17,7 +20,10 @@ public class SortCommand extends Command {
             + "Parameters: f/[FIELD] o/[ORDER]\n"
             + "Example: " + COMMAND_WORD + " f/name o/asc";
 
-    public static final String MESSAGE_SUCCESS = "Sorted all persons";
+    public static final String MESSAGE_SUCCESS = "Sorted all persons by %s in %s order";
+
+    public static final String INVALID_FIELD_MESSAGE = "Invalid field! Valid fields: name, tag";
+    public static final String INVALID_ORDER_MESSAGE = "Invalid order! Use 'asc' or 'desc'";
 
     private String field;
     private String order;
@@ -35,10 +41,11 @@ public class SortCommand extends Command {
         this.order = order;
     }
 
-    // TODO
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        throw new CommandException("NOT IMPLEMENTED YET");
+        Comparator<Person> comparator = getComparator();
+        model.sortPersons(comparator);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, field, getOrderFullFormat()));
     }
 
     @Override
@@ -54,5 +61,52 @@ public class SortCommand extends Command {
         SortCommand other = (SortCommand) o;
 
         return this.field.equals(other.field) && this.order.equals(other.order);
+    }
+
+    private Comparator<Person> getComparator() throws CommandException {
+        Comparator<Person> comparator;
+
+        switch (field) {
+        case "name":
+            comparator = Comparator.comparing(person -> person.getName().toString(), String.CASE_INSENSITIVE_ORDER);
+            break;
+        case "tag":
+            comparator = Comparator.comparing(person ->
+                    person.getTags().stream()
+                          .map(tag -> tag.tagName.toLowerCase())
+                          .sorted()
+                          .findFirst()
+                          .orElse(""));
+            break;
+        default:
+            throw new CommandException(INVALID_FIELD_MESSAGE);
+        }
+
+        switch (order) {
+        case "asc":
+        case "ascending":
+            return comparator;
+        case "desc":
+        case "descending":
+            return comparator.reversed();
+        default:
+            throw new CommandException(INVALID_ORDER_MESSAGE);
+        }
+    }
+
+    /**
+     * Returns the full format of a speicifed order
+     * Precondition: Order is valid and is either an abbreviation or the full format
+     * @return String containing the full format of the order
+     */
+    private String getOrderFullFormat() {
+        switch (order) {
+        case "asc":
+            return "ascending";
+        case "desc":
+            return "descending";
+        default:
+            return order;
+        }
     }
 }
--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -2,7 +2,6 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
@@ -12,25 +11,17 @@ import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 
 public class SortCommandTest {
-
-    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-
-    @Test
-    public void execute() {
-        assertCommandFailure(new SortCommand("names", "asc"), model, "NOT IMPLEMENTED YET");
-    }
-
     @Test
     public void equals() {
-        final SortCommand standardCommand = new SortCommand("names", "asc");
-        SortCommand commandWithSameValues = new SortCommand("names", "asc");
+        final SortCommand standardCommand = new SortCommand("name", "asc");
+        SortCommand commandWithSameValues = new SortCommand("name", "asc");
 
         assertTrue(standardCommand.equals(commandWithSameValues));
         assertTrue(standardCommand.equals(standardCommand));
         assertFalse(standardCommand.equals(null));
         assertFalse(standardCommand.equals(new ClearCommand()));
-        assertFalse(standardCommand.equals(new SortCommand("names", "desc")));
-        assertFalse(standardCommand.equals(new SortCommand("tags", "asc")));
+        assertFalse(standardCommand.equals(new SortCommand("name", "desc")));
+        assertFalse(standardCommand.equals(new SortCommand("tag", "asc")));
 
     }
 }
