a531fdcbd36c0bc90d3fe572fe5f23f651d7c257
joelchongg <joelchongggg@gmail.com>
2025-10-10 11:38:22 +0800

    Fix checkstyle Issues
    Issues arised due to unused imports
    
    Removed the unused imports in order to pass CI

--- src/test/java/seedu/address/logic/commands/SortCommandTest.java
+++ src/test/java/seedu/address/logic/commands/SortCommandTest.java
@@ -2,13 +2,8 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
-
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
 
 public class SortCommandTest {
     @Test
