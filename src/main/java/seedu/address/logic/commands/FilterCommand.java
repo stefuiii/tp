79b83c14eead71c84f5c045385184eb2547c8a0c
joelchongg <joelchongggg@gmail.com>
2025-10-12 17:36:00 +0800

    Add support for filtering
    Filter command can be used to filter contacts based
    on one or more tags.
    
    Tags are checked for edge cases but checks may not be
    exhaustive.
    
    Tests will be added in the next commit.

---
+++ src/main/java/seedu/address/logic/commands/FilterCommand.java
@@ -0,0 +1,55 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.logic.Messages;
+import seedu.address.model.Model;
+import seedu.address.model.tag.TagsContainTagPredicate;
+
+/**
+ * Filters and lists all persons in the address book whose contact information contains any of the tags.
+ * Keyword matching is case insensitive.
+ */
+public class FilterCommand extends Command {
+
+    public static final String COMMAND_WORD = "filter";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Filters all persons whose contact information contains any of "
+            + "the specified tags (case-insensitive) and displays them as a list.\n"
+            + "Parameters: t/[TAG]...\n"
+            + "Example: " + COMMAND_WORD + " t/ friends t/ colleague";
+
+    private final TagsContainTagPredicate predicate;
+
+    /**
+     * Creates a FilterCommand object with a given {@TagsContainTagPredicate} object.
+     * @param predicate {@TagsContainTagPredicate} object to filter by
+     */
+    public FilterCommand(TagsContainTagPredicate predicate) {
+        requireNonNull(predicate);
+        this.predicate = predicate;
+    }
+
+    @Override
+    public CommandResult execute(Model model) {
+        requireNonNull(model);
+        model.updateFilteredPersonList(predicate);
+        return new CommandResult(
+                String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof FilterCommand)) {
+            return false;
+        }
+
+        FilterCommand otherFilterCommand = (FilterCommand) other;
+        return predicate.equals(otherFilterCommand.predicate);
+    }
+}
