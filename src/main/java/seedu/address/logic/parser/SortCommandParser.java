7bbd3b08b80bcebcb46b9ac3e66e832911d5bb16
joelchongg <joelchongggg@gmail.com>
2025-10-19 15:34:46 +0800

    Add logging for SortCommand and SortCommandParser
    There was a lack of logging for SortCommand and SortCommandParser
    
    Added logging for these two classes.
    Logging is included for most methods which are involved in the processing of a command.

--- src/main/java/seedu/address/logic/parser/SortCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SortCommandParser.java
@@ -5,6 +5,9 @@ import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_FIELD;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ORDER;
 
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.logic.commands.SortCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -12,6 +15,8 @@ import seedu.address.logic.parser.exceptions.ParseException;
  * Parses input arguments and creates a new SortCommand object
  */
 public class SortCommandParser implements Parser<SortCommand> {
+
+    private static final Logger logger = LogsCenter.getLogger(SortCommandParser.class);
 
     /**
      * Parses the given {@code String} of arguments in the context of the SortCommand
@@ -21,13 +26,15 @@ public class SortCommandParser implements Parser<SortCommand> {
      */
     public SortCommand parse(String args) throws ParseException {
         requireNonNull(args);
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_FIELD, PREFIX_ORDER);
+        logger.fine("Parsing sort command with arguments: " + args);
 
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_FIELD, PREFIX_ORDER);
         checkValidTokens(argMultimap);
 
         String field = argMultimap.getValue(PREFIX_FIELD).orElse("");
         String order = argMultimap.getValue(PREFIX_ORDER).orElse("");
 
+        logger.info("Successfully parsed sort command - field: " + field + ", order: " + order);
         return new SortCommand(field, order);
     }
 
@@ -49,10 +56,12 @@ public class SortCommandParser implements Parser<SortCommand> {
      */
     private void checkValidTokens(ArgumentMultimap argMultimap) throws ParseException {
         if (!argMultimap.getValue(PREFIX_FIELD).isPresent() || !argMultimap.getValue(PREFIX_ORDER).isPresent()) {
+            logger.warning("Missing required prefix - field or order not provided");
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
         }
 
         if (!argMultimap.getPreamble().isEmpty()) {
+            logger.warning("Preamble found in sort command: " + argMultimap.getPreamble());
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, SortCommand.MESSAGE_USAGE));
         }
 
