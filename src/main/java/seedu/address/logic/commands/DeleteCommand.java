1f2bd074f0aa1245414e1bdd28a84777d7a98cb5
Reineil <fish82ee@gmail.com>
2025-10-23 20:15:19 +0800

    Add UML diagram for Deletion

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -102,6 +102,7 @@ public class DeleteCommand extends Command {
         }
 
         Person personToDelete = lastShownList.get(targetIndex.getZeroBased());
+        assert false;
         model.deletePerson(personToDelete);
         return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, Messages.format(personToDelete)));
     }
