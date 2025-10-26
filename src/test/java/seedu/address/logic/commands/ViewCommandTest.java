37dbb43f16bdfe29a455ccdd258d68a10e234a6a
zhenghao <zhenghao3210@gmail.com>
2025-10-26 13:00:57 +0800

    Add Equality Test ViewCommand

--- src/test/java/seedu/address/logic/commands/ViewCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewCommandTest.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.ViewCommand.MESSAGE_SHOWING;
@@ -39,5 +41,22 @@ public class ViewCommandTest {
         // Out Of Bounds -> Throws Command Exception
         final ViewCommand oobVc = new ViewCommand(0);
         assertThrows(CommandException.class, () -> oobVc.execute(model));
+    }
+
+    @Test
+    public void equalTest() {
+        ViewCommand original = new ViewCommand(1);
+        ViewCommand same = new ViewCommand(1);
+        ViewCommand empty = new ViewCommand();
+        ViewCommand empty2 = new ViewCommand();
+
+        assertEquals(original, original);
+        assertEquals(original, same);
+        assertEquals(empty, empty2);
+
+        // Invalid Equality
+        assertNotEquals(original, empty);
+        assertNotEquals(empty2, same);
+
     }
 }
