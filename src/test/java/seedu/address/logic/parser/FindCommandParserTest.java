cf213601a4466af4ab0b28ccfe6f5172afbab8ac
stefuiii <jianglh0425@outlook.com>
2025-10-30 02:30:42 +0800

    Fix the styling error

--- src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FindCommandParserTest.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.commands.CommandTestUtil.PREAMBLE_WHITESPACE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
@@ -12,7 +11,6 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 
 import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NameOrCompanyPredicate;
 
 /**
